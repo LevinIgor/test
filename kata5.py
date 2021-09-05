cdaef192ebf8ce5b37cacc9d6c62dd0fd82670f2
Igor <57236547+LevinIgor@users.noreply.github.com>
2021-09-05 13:48:37 +0300

    5 kata is done!

--- kata5.py
+++ kata5.py
@@ -1,9 +1,26 @@
 def duplicate_encode(word):
-    return word 
-fdd
+    _duplicate = []
+    word = word.upper()
+    _return_str=""
+    
+    for i in range(len(word)):
+        temp = word[i]
+        for j in range(i+1,len(word)):
+            if temp == word[j]:
+              _duplicate.append(word[j])
+    for i in range(len(word)):
+        if(word[i] in _duplicate):
+            _return_str+=')'
+        else:
+            _return_str+='('
+    
+        
+    return _return_str 
 
 
 
 
 
-print(duplicate_encode("recede"))
+
+print(duplicate_encode("din"))
+#()(((()
