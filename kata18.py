59807aa42b9a8ab4bac7a8b6c69b99a11472242f
Igor <57236547+LevinIgor@users.noreply.github.com>
2021-09-08 18:52:24 +0300

    kata 18 is done!

---
+++ kata18.py
@@ -0,0 +1,18 @@
+def pig_it(text):
+    _list = text.split(' ')
+    _revers = []
+
+    for i in _list:
+        if i in ['!']:
+            _revers.append(i)
+        else:
+            _revers.append((i[1:]+i[0]) + "ay")
+        
+        
+        
+    return ' '.join(_revers)
+
+
+
+
+print(pig_it("Hello world !"))
