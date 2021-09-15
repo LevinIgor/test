e40b965e286d9c065d8c579ca4e43cb5af337976
Ihor <lytghzys@gmail.com>
2021-09-15 10:24:11 +0300

    kata 25 filter_list done

--- kata18.py
+++ kata18.py
@@ -7,12 +7,7 @@ def pig_it(text):
             _revers.append(i)
         else:
             _revers.append((i[1:]+i[0]) + "ay")
-        
-        
-        
+     
     return ' '.join(_revers)
-
-
-
 
 print(pig_it("Hello world !"))
--- kata25.py
+++ kata25.py
@@ -1,3 +1,10 @@
+def filter_list(l):
+  _return = []
 
+  for i in l:
+      if type(i) is type(0):
+          _return.append(i)
+   
+  return _return
 
-
+print(filter_list([1,2,'aasf','1','123',123]))
